9eadcd81de874ae62c32c7890de92b01fe0a6aba
slivering <47292273+slivering@users.noreply.github.com>
2025-08-28 06:31:52 +0200

    Add data module

---
+++ vinasmol/training/cpt.py
@@ -0,0 +1,119 @@
+from dataclasses import dataclass, field
+from pathlib import Path
+from typing import Optional, Union
+
+from torch.utils.data import DataLoader
+
+from litgpt.data import DataModule
+from litgpt.tokenizer import Tokenizer
+
+@dataclass
+class SmolLMDataModule(DataModule):
+    """A mix of Vietnamese, English and code datasets with training and validation dataloaders."""
+
+    data_path: Union[str, Path] = Path("data/")
+    seed: int = 20250828
+    num_workers: int = 8
+
+    batch_size: int = field(init=False, repr=False, default=1)
+    seq_length: int = field(init=False, repr=False, default=2048)
+
+    def __post_init__(self):
+        super().__init__()
+        self.data_path = Path(self.data_path)
+        self.vi_train = self.data_path / "vi-all/train"
+        self.vi_val = self.data_path / "vi-all/val"
+        self.en_train = self.data_path / "en-all/train"
+        self.en_val = self.data_path / "en-all/val"
+        self.code_train = self.data_path / "code-all/train"
+        self.code_val = self.data_path / "code-all/val"
+        self.required_paths = [
+            self.vi_train, self.vi_val,
+            self.en_train, self.en_val,
+            self.code_train, self.code_val,
+        ]
+
+    def connect(
+        self, tokenizer: Optional[Tokenizer] = None, batch_size: int = 1, max_seq_length: Optional[int] = None
+    ) -> None:
+        self.batch_size = batch_size
+        self.seq_length = max_seq_length + 1  # Increase by one because we need the next token as well
+
+    def prepare_data(self) -> None:
+        for path in self.required_paths:
+            if not path.is_dir():
+                raise FileNotFoundError(
+                    "The data path for SmolLM must be a directory with these subdirectories:"
+                    " `en-all/train`, `en-all/val`, `vi-all/train`, `vi-all/val`, `code-all/train`, `code-all/val`."
+                    f" The directory {path} does not exist."
+                    " Set it via `--data.data_path=...`"
+                )
+
+    def train_dataloader(self) -> DataLoader:
+        from litdata.streaming import CombinedStreamingDataset, StreamingDataLoader, StreamingDataset, TokensLoader
+
+        vi_train_data = StreamingDataset(
+            input_dir=self.vi_train,
+            item_loader=TokensLoader(block_size=self.seq_length),
+            shuffle=True,
+            drop_last=True,
+        )
+        en_train_data = StreamingDataset(
+            input_dir=self.en_train,
+            item_loader=TokensLoader(block_size=self.seq_length),
+            shuffle=True,
+            drop_last=True,
+        )
+        code_train_data = StreamingDataset(
+            input_dir=self.code_train,
+            item_loader=TokensLoader(block_size=self.seq_length),
+            shuffle=True,
+            drop_last=True,
+        )
+        train_datasets = [
+            vi_train_data,
+            en_train_data,
+            code_train_data,
+        ]
+
+        # Mix Vietnamese, English and code data with these proportions
+        weights = (0.5, 0.4, 0.1)
+        train_data = CombinedStreamingDataset(
+            datasets=train_datasets, seed=self.seed, weights=weights, iterate_over_all=False
+        )
+
+        train_dataloader = StreamingDataLoader(
+            train_data, batch_size=self.batch_size, pin_memory=True, num_workers=self.num_workers, drop_last=True
+        )
+        return train_dataloader
+
+    def val_dataloader(self) -> DataLoader:
+        from litdata.streaming import StreamingDataLoader, CombinedStreamingDataset, StreamingDataset, TokensLoader
+
+        vi_val_data = StreamingDataset(
+            input_dir=self.vi_val,
+            item_loader=TokensLoader(block_size=self.seq_length),
+            shuffle=True,
+        )
+        en_val_data = StreamingDataset(
+            input_dir=self.en_val,
+            item_loader=TokensLoader(block_size=self.seq_length),
+            shuffle=True,
+        )
+        code_val_data = StreamingDataset(
+            input_dir=self.code_val,
+            item_loader=TokensLoader(block_size=self.seq_length),
+            shuffle=True,
+        )
+        val_datasets = [
+            vi_val_data,
+            en_val_data,
+            code_val_data,
+        ]
+        val_data = CombinedStreamingDataset(
+            datasets=val_datasets, seed=self.seed, iterate_over_all=False
+        )
+        val_dataloader = StreamingDataLoader(
+            val_data, batch_size=self.batch_size, pin_memory=True, num_workers=self.num_workers, drop_last=True
+        )
+        return val_dataloader
--- vinasmol/training/dataset/mix.py
+++ vinasmol/training/dataset/mix.py
@@ -1,72 +1,18 @@
 from functools import partial
-import json
 from pathlib import Path
 
-from datasets import load_dataset, Dataset
-from loguru import logger
+from datasets import load_dataset
+from litdata import optimize
 import pyarrow.parquet as pq
-from litdata import (
-    index_parquet_dataset, merge_datasets, optimize,
-    StreamingDataset, CombinedStreamingDataset,
-    StreamingDataLoader, TokensLoader,
-)
-from tqdm import tqdm
-from transformers import AutoTokenizer, GPT2TokenizerFast, PreTrainedTokenizerFast
+from transformers import GPT2TokenizerFast, PreTrainedTokenizerFast
 import typer
 
-from . import DATA_DIR, DATA_DIR_CODE, DATA_DIR_EN, DATA_DIR_VI
-from .preprocessing import DatasetNames
+from . import DATA_DIR
 from vinasmol.tokenization import DATA_DIR as TOKENIZER_DATA_DIR
 
 
 SEED = 20250827
-CHAR_BUDGET = 6_000_000_000 # 6B chars ~ 2B tokens
 
-TARGET_WEIGHTS_EN = {
-    DatasetNames.fineweb_edu_dedup: 0.7,
-    DatasetNames.gutenberg_en: 0.2,
-    DatasetNames.wikipedia_en: 0.1,
-}
-
-TARGET_WEIGHTS_CODE = {
-    DatasetNames.starcoder_python_edu: 1.0,
-}
-
-TARGET_WEIGHTS_EN_ANNEALING = {
-    DatasetNames.cosmopedia_v2: 0.55,
-    DatasetNames.olmocr_pes2o: 0.2,
-    DatasetNames.flan_v2: 0.1,
-    DatasetNames.stackmathqa: 0.05,
-    DatasetNames.open_web_math: 0.05,
-    DatasetNames.mathpile_commercial: 0.05,
-}
-
-TARGET_WEIGHTS_VI = {
-    DatasetNames.wikipedia_vi: 0.1,
-    DatasetNames.fineweb2_hq: 0.25,
-    DatasetNames.culturax: 0.25,
-    DatasetNames.madlad400: 0.15,
-    DatasetNames.binhvq_news_corpus: 0.2,
-    DatasetNames.mtet: 0.03,
-    DatasetNames.vbpl: 0.02,
-    #DatasetNames.ccvj: # Keep for annealing
-}
-
-def compute_weights(
-        dataset_folders: list[Path],
-        target_weights: dict[str, float],
-        char_budget: int = CHAR_BUDGET,
-    ) -> dict[Path, float]:
-    weights = {}
-    for folder in dataset_folders:
-        ds = load_dataset(data_dir=f"{folder}", split='train', num_proc=4)
-        total_chars = sum(len(row['text']) for row in ds)
-        origin = ds['metadata'][0]['origin']
-        current_weight = total_chars / char_budget
-        target_weight = target_weights[origin] / sum(target_weights.values())
-        weights[folder] = target_weight / current_weight
-    return weights
-    
 def tokenize_fn(filepath: str, tokenizer: PreTrainedTokenizerFast):
     parquet_file = pq.ParquetFile(filepath)
     # Process per batch to reduce RAM usage
@@ -75,10 +21,23 @@ def tokenize_fn(filepath: str, tokenizer: PreTrainedTokenizerFast):
             # SmolLM does not have BOS by default?
             yield tokenizer.encode(text, add_special_tokens=True)
 
+def split_parquet(data_dir: Path, out_dir: Path):
+    ds = load_dataset('parquet', data_dir=f"{data_dir}")
+    train_split = ds.train_test_split(
+        test_size=0.2,
+        seed=SEED
+    )
+    ds_train = train_split['train']
 
-def main(
-        char_budget: int = CHAR_BUDGET,
-):
+    test_split = train_split['test'].train_test_split(test_size=0.5)
+    ds_val = test_split['train']
+    ds_test = test_split['test']
+    ds_train.to_parquet(f"{out_dir / 'train.parquet'}")
+    ds_val.to_parquet(f"{out_dir / 'val.parquet'}")
+    ds_test.to_parquet(f"{out_dir / 'test.parquet'}")
+
+
+def main():
     tokenizer = GPT2TokenizerFast.from_pretrained(TOKENIZER_DATA_DIR / 'merged')
 
     data_dirs = [
@@ -87,16 +46,23 @@ def main(
         DATA_DIR / "code-all" / "deduped",
     ]
 
+    
     for data_dir in data_dirs:
-        inputs = [str(file) for file in Path(data_dir).rglob("*.parquet")]
+        out_dir = data_dir.parent / "splits"
+        split_parquet(data_dir, out_dir=out_dir)
+
+    for data_dir in data_dirs:
+        inputs = [str(file) for file in Path(data_dir).rglob("*splits/*.parquet")]
 
         outputs = optimize(
             fn=partial(tokenize_fn, tokenizer=tokenizer),
             inputs=inputs,
             output_dir=f"{data_dir.parent}/optimized",
             # sequence length of 2048 before context length extension
-            chunk_size=(2049 * 128),
+            chunk_size=(2049 * 127),
         )
+
+        # TODO: restructure file hierarchy : corpus / split / file
 
 if __name__ == '__main__':
     typer.run(main)
